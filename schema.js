210d8067ea43e48f7b8ef92649715e153732e1e8
uran-pd@ <88038272+uran-pd@users.noreply.github.com>
2022-03-28 00:08:35 +0530

    schema
    customize module in register validate schema

---
+++ schema.js
@@ -0,0 +1,29 @@
+// const mongoose = require('mongoose')
+// const validator = require('validator');
+
+// const userSchema = new mongoose.Schema({
+//     username: {
+//         type: String,
+//         required: true,
+//         maxLength: 10
+//     },
+//     email: {
+//         type: String,
+//         trim: true,
+//         lowercase: true,
+//         unique: true,
+//         validate: {
+//             validator: function (v) {
+//                 return /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/.test(v);
+//             },
+//             message: "Please enter a valid email"
+//         },
+//         required: [true, "Email required"]
+//     },
+//     password: {
+//         type: String,
+//         required: true
+//     }
+// })
+
+// module.exports = mongoose.model('user', userSchema)
